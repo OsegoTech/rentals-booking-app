a30dbdb7d59995e4faa077ffc33a0dbc0f22f466
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-07-23 14:35:46 +0300

    finalized the api

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,68 +1,80 @@
 import RoomModel from "../models/RoomModel.js";
 import Hotel from "../models/HotelModel.js";
 
-export  const createRoom = async (req, res, next) => {
-    const hotelId = req.params.hotelid
-    const newRoom = new RoomModel(req.body)
+export const createRoom = async (req, res, next) => {
+  const hotelId = req.params.hotelid;
+  const newRoom = new RoomModel(req.body);
+  try {
+    const savedRoom = await newRoom.save();
     try {
-        const savedRoom = await  newRoom.save()
-        try{
-            await Hotel.findByIdAndUpdate(hotelId, {$push : {rooms : savedRoom._id}})
-        } catch (err){
-            next(err)
-        }
-
-        res.status(200).json(savedRoom)
-    } catch (err){
-        next(err)
-    }
-}
-
-export const updateRoom = async(req, res, next) => {
-    try {
-        const updatedRoom = await RoomModel.findByIdAndUpdate(
-            req.params.id,
-            { $set: req.body },
-            { new: true }
-        );
-        res.status(200).json(updatedRoom);
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $push: { rooms: savedRoom._id },
+      });
     } catch (err) {
-        next(err)
+      next(err);
     }
-}
 
-export const deleteRoom = async(req, res, next) => {
-    const hotelId = req.params.hotelid
+    res.status(200).json(savedRoom);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateRoom = async (req, res, next) => {
+  try {
+    const updatedRoom = await RoomModel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedRoom);
+  } catch (err) {
+    next(err);
+  }
+};
+export const updateRoomAvailability = async (req, res, next) => {
+  try {
+    await RoomModel.updateOne(
+      { "roomNumbers._id": req.params.id },
+      { $push: { "roomNumbers.$.unavailableDates": req.body.dates } }
+    );
+    res.status(200).json("Room availability has been updated");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteRoom = async (req, res, next) => {
+  const hotelId = req.params.hotelid;
+  try {
+    await RoomModel.findByIdAndDelete(req.params.id);
     try {
-        await RoomModel.findByIdAndDelete(req.params.id)
-        try{
-            await Hotel.findByIdAndUpdate(hotelId, {
-                $pull : {rooms : req.params.id},
-            });
-        } catch (err){
-            next(err)
-        }
-        res.status(200).json("Room has been deleted");
-
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $pull: { rooms: req.params.id },
+      });
     } catch (err) {
-        next(err)
+      next(err);
     }
-}
+    res.status(200).json("Room has been deleted");
+  } catch (err) {
+    next(err);
+  }
+};
 
-export const getRoom = async(req, res, next) => {
-    try {
-        const room = await RoomModel.findById(req.params.id);
-        res.status(200).json(room);
-    } catch (err) {
-        next(err)
-    }
-}
+export const getRoom = async (req, res, next) => {
+  try {
+    const room = await RoomModel.findById(req.params.id);
+    res.status(200).json(room);
+  } catch (err) {
+    next(err);
+  }
+};
 
-export const getRooms = async(req, res, next) => {
-    try {
-        const rooms = await RoomModel.find();
-        res.status(200).json(rooms);
-    } catch (err) {
-        next(err)
-    }
-}
+export const getRooms = async (req, res, next) => {
+  try {
+    const rooms = await RoomModel.find();
+    res.status(200).json(rooms);
+  } catch (err) {
+    next(err);
+  }
+};
--- routes/roomsRoute.js
+++ routes/roomsRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {verifyAdmin} from "../utils/verifyToken.js";
-import {createRoom, deleteRoom, getRoom, getRooms, updateRoom} from "../controllers/roomController.js";
+import {createRoom, deleteRoom, getRoom, getRooms, updateRoom, updateRoomAvailability} from "../controllers/roomController.js";
 
 const router = express.Router();
 
@@ -9,6 +9,7 @@ router.post("/:hotelid", verifyAdmin, createRoom)
 
 // UPDATE
 router.put("/:id", verifyAdmin, updateRoom)
+router.put("/availability/:id", updateRoomAvailability)
 
 // DELETE
 router.delete("/:id/:hotelid", verifyAdmin, deleteRoom)
